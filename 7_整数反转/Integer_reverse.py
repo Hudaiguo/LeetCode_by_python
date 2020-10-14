8f76f8bc3fbb926d149d11f5e28363b2ec2a39f2
hudaiguo <suiyueliusha2013@163.com>
2020-10-14 23:19:38 +0800

    7_整数翻转

---
+++ 7_整数反转/Integer_reverse.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+@Time: 2020/10/14 22:25 
+@Author: Hudaiguo
+@python version: 3.5.2
+#整数反转
+#-170 -> -71
+#170 -> 71
+#123 -> 321
+#超过32位存储返回0
+#44ms 13.6M
+"""
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x == 0:
+            return 0
+
+        x = str(x)
+        if x[0] == "-" or x[0] == "+":
+            while x[-1] == "0":
+                x = x[:-1]
+            y = int(x[0] + x[1:][::-1])
+        elif x[-1] != "0":
+            y = int(x[::-1])
+        else:
+            while x[-1] == "0":
+                x = x[:-1]
+            y =  int(x[::-1])
+
+        if -2**31-1 < y < 2**31:
+            return y
+        else:
+            return 0
+
+Integer = Solution()
+print(Integer.reverse(1534236469))
